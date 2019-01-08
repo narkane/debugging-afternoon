de01b4470262f0273675ac5f40a90587914838d5
Hans Thummel <enikath@gmail.com>
2019-01-08 16:26:22 -0600

    finished

--- stage-2/src/components/Home/Home.js
+++ stage-2/src/components/Home/Home.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
-import './Home.css';
+import React, { Component } from "react";
+import "./Home.css";
 
-class Home extends Component {
-    render() {
-        return (
-            <div className="home-landing"></div>
-        )
-    }
+export default class Home extends Component {
+  render() {
+    return <div className="home-landing" />;
+  }
 }
